03b54c96bdc3bdd43ba61f50e90f48150402b4ab
unintended000 <anechka187@mail.ru>
2017-05-23 23:01:24 +0300

    Add files via upload

---
+++ 1/GET/Regestration_action.php
@@ -0,0 +1,12 @@
+
+<html>
+<?php
+$username=$_GET['username'];
+$mail=$_GET['email'];
+$password=$_GET['password'];
+
+echo "<p>Username =" . $username . "</p>";
+echo "<p>Mail = " . $mail. "</p>";
+echo "<p>Password = " . $password . "</p>";
+?>
+</html>
---
+++ 1/POST/Regestration_action.php
@@ -0,0 +1,12 @@
+
+<html>
+<?php
+$username=$_POST['username'];
+$mail=$_POST['email'];
+$password=$_POST['password'];
+
+echo "<p>Username =" . $username . "</p>";
+echo "<p>Mail = " . $mail. "</p>";
+echo "<p>Password = " . $password . "</p>";
+?>
+</html>
